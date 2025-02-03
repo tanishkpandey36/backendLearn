df69f6d409df6fd7c46eaed93cffe27913787d2a
taniskpandey36 <tanishkkumar@rungta.edu.in>
2025-02-03 07:16:37 +0530

    creating routes

--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
 
 //require('dotenv').config({path:'./env'})
 import dotenv from "dotenv";
-import express from "express";
 import connectDB from "./db/db.js";
-const app = express()
+import {app} from './app.js'
 
 dotenv.config({
     path:'./env'
